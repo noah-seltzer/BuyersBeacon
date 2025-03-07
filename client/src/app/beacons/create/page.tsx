88f7c8d20602de79cb53aa480bb1e215058ab3c2
Rshokar <60485121+Rshokar@users.noreply.github.com>
2025-03-07 14:00:40 -0800

    Updated client controller to send form data (#11)
    * Updated client controller to send formdata
    
    * Fixed coments
    
    * Fixed coments

--- client/src/app/beacons/create/page.tsx
+++ client/src/app/beacons/create/page.tsx
@@ -34,7 +34,6 @@ const CreateBeaconPage: FC = () => {
       // THIS IS WHERE WE WILL upload the image to S3 first and then send the URL with the beacon data
       helpers.setSubmitting(true)
       const res = await createBeacon(beacon).unwrap();
-      console.log(res);
       navigateToBeaconDetailsPage(router, res);
     } catch (error) {
       console.error("Failed to create beacon:", error);
