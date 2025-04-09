28bf007ca9e2ac7ced4807eeefb0b51631d12075
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2025-04-09 13:45:57 -0700

    Hotfix(client): profile page now correctly only shows users own beacons

--- server/Services/BeaconService.cs
+++ server/Services/BeaconService.cs
@@ -62,13 +62,8 @@ namespace server.Services
                 query = query.Where(b => b.ItemName.Contains(QueryString));
             }
 
-                if (!String.IsNullOrEmpty(QueryString))
-                {
-                    query = query.Where(b => b.ItemName.Contains(QueryString));
-                }
-
-                query = query.Where(b => b.IsDraft == drafts);
-            }
+            query = query.Where(b => b.IsDraft == drafts);
+            
             return await query.ToListAsync();
         }
 
