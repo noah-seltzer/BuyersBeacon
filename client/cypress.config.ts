277007244bde6205aca71ed24473b4ceb40ac98d
Noah Seltzer <noahseltzer@outlook.com>
2025-04-15 19:47:36 -0700

    fix(build):fix cypress config

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,13 +1,9 @@
-import { clerkSetup } from '@clerk/testing/cypress'
 import { defineConfig } from 'cypress'
 require('dotenv').config();
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      config.env.BASEURL = process.env.CYPRESS_BASE_URL
-      return clerkSetup({ config })
-    },
+    baseUrl: process.env.CYPRESS_BASE_URL,
     supportFile: false,
   },
 })
